6cce417167db5bb5bd40a71b6aeb0110962d3b27
Employee 451 <mail@askehippebrun.com>
2018-03-26 10:35:44 +0200

    ES6

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# TriCalc
-## Trigonometric Calculator.
+# TriCalc – Trigonometric Calculator
+## Explanation
 Takes in known properties of right-angle triangles, and spits out the rest.
 
-Based on a deign from secion 4 "Pig Game" of Jonas Schmedtmann's Udemy course "The Complete JavaScript Course: Build a Real-World Project."
+Based on a design from section 4 "Pig Game" of Jonas Schmedtmann's Udemy course "The Complete JavaScript Course: Build a Real-World Project."
--- app.js
+++ app.js
@@ -1,136 +1,193 @@
-init();
+const mathController = (() => {
 
-document.querySelector('.btn-calc').addEventListener('click', function() {
-  var triangle = {
-    angleA: document.getElementById('angle-a').value,
-    angleB: document.getElementById('angle-b').value,
-    sideA: document.getElementById('side-a').value,
-    sideB: document.getElementById('side-b').value,
-    sideC: document.getElementById('side-c').value
-  }
+  const toRadians = (angle) => angle * (Math.PI / 180);
 
-  calculateTriangle(triangle);
-});
+  const toDegrees = (angle) => angle * (180 / Math.PI);
 
-document.querySelector('.btn-new').addEventListener('click', function() {
-  init();
-});
+  return {
+    calculateTriangle: (triangle) => {
+      triangle.angleC = 90;
 
-function calculateTriangle(triangle) {
-  triangle.angleC = 90;
-
-  if( !triangle.angleA ) {
-    if( triangle.angleB ) {
-      triangle.angleA = 90 - triangle.angleB;
-    } else if( triangle.sideA ) {
-      if( triangle.sideB ) {
-        triangle.angleA = toDegrees( Math.atan( triangle.sideA / triangle.sideB ) );
-      } else if( triangle.sideC ) {
-        triangle.angleA = toDegrees( Math.asin( triangle.sideA / triangle.sideC ) );
+      if( !triangle.angleA ) {
+        if( triangle.angleB ) {
+          triangle.angleA = 90 - triangle.angleB;
+        } else if( triangle.sideA ) {
+          if( triangle.sideB ) {
+            triangle.angleA = toDegrees( Math.atan( triangle.sideA / triangle.sideB ) );
+          } else if( triangle.sideC ) {
+            triangle.angleA = toDegrees( Math.asin( triangle.sideA / triangle.sideC ) );
+          }
+        } else if( triangle.sideB ) {
+          if( triangle.sideC ) {
+            triangle.angleA = toDegrees( Math.acos( triangle.sideB / triangle.sideC ) );
+          }
+        }
       }
-    } else if( triangle.sideB ) {
-      if( triangle.sideC ) {
-        triangle.angleA = toDegrees( Math.acos( triangle.sideB / triangle.sideC ) );
+
+      if( !triangle.angleB ) {
+        if( triangle.angleA ) {
+          triangle.angleB = 90 - triangle.angleA;
+        } else if( triangle.sideA ) {
+          if( triangle.sideB ) {
+            triangle.angleB = toDegrees( Math.atan( triangle.sideB / triangle.sideA ) );
+          } else if( triangle.sideC ) {
+            triangle.angleB = toDegrees( Math.acos( triangle.sideA / triangle.sideC ) );
+          }
+        } else if( triangle.sideB ) {
+          if( triangle.sideC ) {
+            triangle.angleB = toDegrees( Math.asin( triangle.sideB / triangle.sideC ) );
+          }
+        }
       }
+
+      if( !triangle.sideA ) {
+        if( triangle.angleA ) {
+          if( triangle.sideB ) {
+            triangle.sideA = Math.tan( toRadians( triangle.angleA ) ) * triangle.sideB;
+          } else if( triangle.sideC ) {
+            triangle.sideA = Math.sin( toRadians( triangle.angleA ) ) * triangle.sideC;
+          }
+        } else if( triangle.angleB ) {
+          if( triangle.sideB ) {
+            triangle.sideA = triangle.sideB / Math.tan( toRadians( triangle.angleB ) );
+          } else if( triangle.sideC ) {
+            triangle.sideA = Math.cos( toRadians( triangle.angleB ) ) * triangle.sideC;
+          }
+        } else if( triangle.sideB ) {
+          if( triangle.sideC ) {
+            triangle.sideA = Math.sqrt( Math.pow( triangle.sideC, 2 ) - Math.pow( triangle.sideB, 2 ) );
+          }
+        }
+      }
+
+      if ( !triangle.sideB ) {
+        if( triangle.angleA ) {
+          if( triangle.sideA ) {
+            triangle.sideB = triangle.sideA / Math.tan( toRadians( triangle.angleA ) );
+          } else if( triangle.sideC ) {
+            triangle.sideB = Math.sin( toRadians( triangle.angleA ) ) * triangle.sideC;
+          }
+        } else if( triangle.angleB ) {
+          if( triangle.sideA ) {
+            triangle.sideB = Math.tan( toRadians( triangle.angleB ) ) * triangle.sideA;
+          } else if( triangle.sideC ) {
+            triangle.sideB = Math.sin( toRadians( triangle.angleB ) ) * triangle.sideC;
+          }
+        } else if( triangle.sideA ) {
+          if( triangle.sideC ) {
+            triangle.sideB = Math.sqrt( Math.pow( triangle.sideC, 2) - Math.pow( triangle.sideA, 2) );
+          }
+        }
+      }
+
+      if ( !triangle.sideC ) {
+        if( triangle.angleA ) {
+          if( triangle.sideA ) {
+            triangle.sideC = triangle.sideA / Math.sin( toRadians( triangle.angleA ) );
+          } else if( triangle.sideB ) {
+            triangle.sideC = triangle.sideB / Math.cos( toRadians( triangle.angleA ) );
+          }
+        } else if( triangle.angleB ) {
+          if( triangle.sideA ) {
+            triangle.sideC = triangle.sideA / Math.cos( toRadians( triangle.angleB ) );
+          } else if( triangle.sideB ) {
+            triangle.sideC = triangle.sideB / Math.sin( toRadians( triangle.angleB ) );
+          }
+        } else if( triangle.sideA ) {
+          if( triangle.sideB ) {
+            triangle.sideC = Math.sqrt( Math.pow( triangle.sideA, 2 ) + Math.pow( triangle.sideB, 2 ) );
+          }
+        }
+      }
+
+      return triangle;
+    }
+  };
+
+})();
+
+const UIController = (() => {
+
+  const DOMElements = {
+    angles: {
+      a: document.getElementById('angle-a'),
+      b: document.getElementById('angle-b')
+    },
+
+    sides: {
+      a: document.getElementById('side-a'),
+      b: document.getElementById('side-b'),
+      c: document.getElementById('side-c')
+    },
+
+    buttons: {
+      calc: document.querySelector('.btn-calc'),
+      new: document.querySelector('.btn-new')
     }
   }
-  if( !triangle.angleB ) {
-    if( triangle.angleA ) {
-      triangle.angleB = 90 - triangle.angleA;
-    } else if( triangle.sideA ) {
-      if( triangle.sideB ) {
-        triangle.angleB = toDegrees( Math.atan( triangle.sideB / triangle.sideA ) );
-      } else if( triangle.sideC ) {
-        triangle.angleB = toDegrees( Math.acos( triangle.sideA / triangle.sideC ) );
-      }
-    } else if( triangle.sideB ) {
-      if( triangle.sideC ) {
-        triangle.angleB = toDegrees( Math.asin( triangle.sideB / triangle.sideC ) );
-      }
-    }
-  }
-  if( !triangle.sideA ) {
-    if( triangle.angleA ) {
-      if( triangle.sideB ) {
-        triangle.sideA = Math.tan( toRadians( triangle.angleA ) ) * triangle.sideB;
-      } else if( triangle.sideC ) {
-        triangle.sideA = Math.sin( toRadians( triangle.angleA ) ) * triangle.sideC;
-      }
-    } else if( triangle.angleB ) {
-      if( triangle.sideB ) {
-        triangle.sideA = triangle.sideB / Math.tan( toRadians( triangle.angleB ) );
-      } else if( triangle.sideC ) {
-        triangle.sideA = Math.cos( toRadians( triangle.angleB ) ) * triangle.sideC;
-      }
-    } else if( triangle.sideB ) {
-      if( triangle.sideC ) {
-        triangle.sideA = Math.sqrt( Math.pow( triangle.sideC, 2 ) - Math.pow( triangle.sideB, 2 ) );
-      }
-    }
-  }
-  if ( !triangle.sideB ) {
-    if( triangle.angleA ) {
-      if( triangle.sideA ) {
-        triangle.sideB = triangle.sideA / Math.tan( toRadians( triangle.angleA ) );
-      } else if( triangle.sideC ) {
-        triangle.sideB = Math.sin( toRadians( triangle.angleA ) ) * triangle.sideC;
-      }
-    } else if( triangle.angleB ) {
-      if( triangle.sideA ) {
-        triangle.sideB = Math.tan( toRadians( triangle.angleB ) ) * triangle.sideA;
-      } else if( triangle.sideC ) {
-        triangle.sideB = Math.sin( toRadians( triangle.angleB ) ) * triangle.sideC;
-      }
-    } else if( triangle.sideA ) {
-      if( triangle.sideC ) {
-        triangle.sideB = Math.sqrt( Math.pow( triangle.sideC, 2) - Math.pow( triangle.sideA, 2) );
-      }
-    }
-  }
-  if ( !triangle.sideC ) {
-    if( triangle.angleA ) {
-      if( triangle.sideA ) {
-        triangle.sideC = triangle.sideA / Math.sin( toRadians( triangle.angleA ) );
-      } else if( triangle.sideB ) {
-        triangle.sideC = triangle.sideB / Math.cos( toRadians( triangle.angleA ) );
-      }
-    } else if( triangle.angleB ) {
-      if( triangle.sideA ) {
-        triangle.sideC = triangle.sideA / Math.cos( toRadians( triangle.angleB ) );
-      } else if( triangle.sideB ) {
-        triangle.sideC = triangle.sideB / Math.sin( toRadians( triangle.angleB ) );
-      }
-    } else if( triangle.sideA ) {
-      if( triangle.sideB ) {
-        triangle.sideC = Math.sqrt( Math.pow( triangle.sideA, 2 ) + Math.pow( triangle.sideB, 2 ) );
-      }
-    }
-  }
 
-  if( !triangle.angleA || !triangle.angleB || !triangle.sideA || !triangle.sideB || !triangle.sideC ) {
-    alert('Not enough information was supplied. You need at least an angle and a side or two sides.');
-  } else {
-    document.getElementById('angle-a').value = triangle.angleA;
-    document.getElementById('angle-b').value = triangle.angleB;
-    document.getElementById('side-a').value = triangle.sideA;
-    document.getElementById('side-b').value = triangle.sideB;
-    document.getElementById('side-c').value = triangle.sideC;
-  }
-}
+  return {
+    resetValues: () => {
+      DOMElements.angles.a.value = '';
+      DOMElements.angles.b.value = '';
 
-function init() {
-  document.getElementById('angle-a').value = '';
-  document.getElementById('angle-b').value = '';
-  document.getElementById('side-a').value = '';
-  document.getElementById('side-b').value = '';
-  document.getElementById('side-c').value = '';
-}
+      DOMElements.sides.a.value = '';
+      DOMElements.sides.b.value = '';
+      DOMElements.sides.c.value = '';
+    },
 
-// Functions
-function toRadians(angle) {
-  return angle * (Math.PI / 180);
-}
+    readValues: () => {
+      return {
+        angleA: DOMElements.angles.a.value,
+        angleB: DOMElements.angles.b.value,
+        sideA: DOMElements.sides.a.value,
+        sideB: DOMElements.sides.b.value,
+        sideC: DOMElements.sides.c.value
+      }
+    },
 
-function toDegrees(angle) {
-  return angle * (180 / Math.PI);
-}
+    setValues: (triangle) => {
+      DOMElements.angles.a.value = triangle.angleA;
+      DOMElements.angles.b.value = triangle.angleB;
+      DOMElements.sides.a.value = triangle.sideA;
+      DOMElements.sides.b.value = triangle.sideB;
+      DOMElements.sides.c.value = triangle.sideC;
+    },
+
+    getDOMElements: () => DOMElements
+  };
+
+})();
+
+const appController = ((mathCtrl, UICtrl) => {
+
+  const calculateTriangle = () => {
+    let triangle = UICtrl.readValues();
+
+    triangle = mathCtrl.calculateTriangle(triangle);
+
+    if( !triangle.angleA || !triangle.angleB || !triangle.sideA || !triangle.sideB || !triangle.sideC ) {
+      alert('Not enough information was supplied. You need at least an angle and a side or two sides.');
+    } else {
+      UICtrl.setValues(triangle);
+    }
+  };
+
+  const setupEventListeners = () => {
+    const DOMElements = UICtrl.getDOMElements();
+
+    DOMElements.buttons.calc.addEventListener('click', calculateTriangle);
+
+    DOMElements.buttons.new.addEventListener('click', appController.init);
+  };
+
+  return {
+    init: () => {
+      UICtrl.resetValues();
+      setupEventListeners();
+    }
+  };
+
+})(mathController, UIController);
+
+appController.init();
